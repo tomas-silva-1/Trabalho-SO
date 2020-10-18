d98a897473e0d2ed71a1a862f5a92c66dc8902b8
tomas-silva-1 <61935208+tomas-silva-1@users.noreply.github.com>
2020-10-18 16:42:13 +0100

    created class medico and finished paciente

---
+++ cria_pacientes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cat /etc/passwd | awk '{print "<",$1,">;",$3}'
+#tail /etc/passwd | cut -d ':' -f1,5
+tail /etc/passwd | awk -F ':' ' {print $1";"$5";;;"$1"@iscte-iul.pt;100"}' | sed -E "s/a//" | sed "s/,,,//"
+#<Nº registo (nº identificação)>;<nome>;<localidade>;<nº telemóvel>;<e-mail>;<saldo>
+#echo "<>;<>;<>;<>;<>;<>"
