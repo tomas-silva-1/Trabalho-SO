4096a6cd8804c9e62711caa9f2021e866a36e402
tomas-silva-1 <61935208+tomas-silva-1@users.noreply.github.com>
2020-10-19 15:19:53 +0100

    finished everything v1

--- cria_pacientes.sh
+++ cria_pacientes.sh
@@ -1,1 +1,1 @@
-head /etc/passwd | awk -F ':' ' {print $1";"$5";;;"$1"@iscte-iul.pt;100"}' | sed -E "s/a//" | sed "s/,,,//" > pacientes.txt
+cat /etc/passwd | awk -F ':' '/^a[0-9]/ { print $1";"$5";;;"$1"@iscte-iul.pt;100"}' | head | sed -E "s/a//" | sed "s/,,,//" > pacientes.txt
