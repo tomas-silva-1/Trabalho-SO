cef6b64c976565108770686e2337e8792e5f8285
tomas-silva-1 <61935208+tomas-silva-1@users.noreply.github.com>
2020-11-15 14:08:40 +0000

    Update cliente.c

---
+++ parte-2/cliente.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int getinfotipo(){
+    int tipo;
+    printf("Indique a descriçao da consulta:\n");
+    scanf("%d",&tipo);
+    
+};
+char getinfodescricao(){
+    printf("Indique a descriçao da consulta:");
+
+    
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
