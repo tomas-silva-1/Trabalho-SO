ba91dc661bb35b4d0e3fd03f0dc877c6545a6c0a
tomas-silva-1 <tomassilva0210@gmail.com>
2020-12-07 12:22:01 +0000

    inicio parte3

---
+++ parte-3/Consulta.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#ifndef __CONSULTA_H__
+#define __CONSULTA_H__
+
+#define TIME 10
+#define LISTA_SIZE 10
+#define P_CONSULTAS "PedidoConsulta.txt"
+#define SRV_CONSULTAS "SrvConsultas.pid"
+
+typedef struct {
+ int tipo; 
+ char descricao[100]; 
+ int pid_consulta; 
+ int status;
+} Consulta;
+
+#endif
